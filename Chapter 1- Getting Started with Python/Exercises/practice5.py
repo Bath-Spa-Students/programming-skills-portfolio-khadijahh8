c55dc24f529d524bea708efeb3bf7eae75cf4c8c
khadijahh8 <khadijahassan777b@gmail.com>
2023-10-03 21:19:01 +0400

    Create practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -0,0 +1,6 @@
+#write four strings in diffrent variables and print the output as one string (string concatenation)
+name = "khadija"
+age= "18"
+school = "bathspa"
+gender = "femal"
+print (name + age + school + gender)
